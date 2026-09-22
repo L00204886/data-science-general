51ca2af1b3a2dba35f97ee66b0bfaf9a5bc385aa
Eamon Foy <esamon.foy@sita.aero>
2026-09-22 20:25:16 +0100

    factors

--- Week2Demo/Week2Demo.R
+++ Week2Demo/Week2Demo.R
@@ -87,9 +87,12 @@ c(10, 150, 30, 40, 55.6) -> x
 1/x
 x+2
 
+x <- c(10, 150, 30, 40, 55.6)
+
 y <-c(x, 0, x)
 y
 
+z <- 2*x + y + 1
 
 p <-c (3,5, 6, 8)
 q <-c (3,3, 3)
@@ -102,3 +105,78 @@ library(swirl)
 
 swirl
 
+
+## Vector Arithmetic
+
+
+x <-c(10.4, 5.6, 3.1, 6.4, 21.7)
+y <-c(x, 0, x)
+z <-2*x + y + 1
+
+
+min(z)
+min(z)
+
+var(x)
+
+# Regular Seq
+
+1:30
+2*1:15
+30:1
+
+seq( 1,30)
+seq(from=1,to=30,by=2)
+
+seq(-5, 5, by=.2) -> s3
+s4 <-seq(length=51, from=-5, by=.2)
+s4 <-seq(from=-100,to=100, by=.6)
+
+# Seq example
+seq3 <- seq(-100, 100, by=0.6)
+Seq3 <- mean((seq3))
+# Sort seq3 in descending order
+sort(seq3, decreasing = TRUE)
+
+
+seq(-5, 5, by=.2) -> s3
+s4 <-seq(length=51, from=-5, by=.2)
+
+
+y <- c(1, 4, 6, 9)
+x <- seq(from=0, to=20, along.with=y)
+
+# Rep
+x <-c(10.4, 5.6, 3.1, 6.4, 21.7)
+s5 <-rep(x, times=5)
+s6 <-rep(x, each=5)
+
+
+# Logical vectors
+x <-c(10.4, 5.6, 3.1, 6.4, 21.7)
+temp <- x > 13
+
+
+# NA
+is.na(seq3)
+z <-c(1:3,NA)
+result <-is.na(z)
+
+
+# Index
+
+y <-x[-(1:4)]
+
+#Factor vectors
+vec1 <-c("Hockey", "Football", "Baseball", "Curling", "Rugby",
+         "Hurling", "Basketball", "Tennis", "Cricket", "Lacrosse")
+vec2 <-c(vec1, "Hockey", "Lacrosse", "Hockey", "Water Polo", "Hockey",
+         "Lacrosse")
+vec2
+vec3 <-vec2 [c(1,3,6)]
+vec3
+vec3_factor <-as.factor(vec3)
+class(vec3)
+
+factor (x=c("Degree", "High School", "Masters", "Doctorate"), levels = c("High
+School", "Degree", "Masters", "Doctorate"), ordered = TRUE)
